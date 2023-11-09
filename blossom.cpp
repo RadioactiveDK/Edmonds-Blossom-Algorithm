63d0552cafa55e6b7096116a659308e836ee607f
Dhruv Kukreja <94745898+RadioactiveDK@users.noreply.github.com>
2023-11-09 16:18:10 +0530

    Update blossom.cpp

--- blossom.cpp
+++ blossom.cpp
@@ -1,167 +1,132 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-const int M = 10;
+const int M = 50;
 
-struct struct_edge
-{
-  int v;
-  struct_edge *n;
-};
-
-struct_edge pool[M * M * 2];
-struct_edge * adj[M];
-struct_edge * top = pool;
-
-
-
-int V;
 queue<int> q;
-
-vector<int> match(M,-1);
-vector<int> parent(M,-1);
-
-int blossom[M];
+vector<int> match(M,-1), parent(M,-1), blossom(M);
 vector<bool> BFS_Tree(M,false);
-vector<bool> in_blossom(M,false);
-
-
-void add_edge(int u, int v){
-  top->v = v, top->n = adj[u], adj[u] = top++;
-  top->v = u, top->n = adj[v], adj[v] = top++;
-}
 
 int LCA(int root, int u, int v){
-  static vector<bool> inp(M,false);
-  while (1)
-  {
-    inp[u = blossom[u]] = true;
-    if (u == root)
-      break;
+  vector<bool> vis(M,false);
+  vis[u]=true;
+  while (1)  {
+    u = blossom[u];
+    vis[u] = true;
+    if (u == root) break;
     u = parent[match[u]];
   }
-  while (1)
-  {
-    if (inp[v = blossom[v]])
-      return v;
-    else
-      v = parent[match[v]];
+  while (1){
+    v = blossom[v];
+    if (vis[v])  return v;
+    else v = parent[match[v]];
   }
 }
 
-void mark_blossom(int lca, int u){
-  while (blossom[u] != lca)
-  {
+void mark_blossom(vector<bool> &in_blssm, int lca, int u){
+  while (blossom[u] != lca)  {
     int v = match[u];
-    in_blossom[blossom[u]] = in_blossom[blossom[v]] = true;
+    in_blssm[blossom[u]] = in_blssm[blossom[v]] = true;
     u = parent[v];
-    if (blossom[u] != lca)
-      parent[u] = v;
+    // if (blossom[u] != lca)  parent[u] = v;
   }
 }
 
-void blossom_contraction(int s, int u, int v){  
+void blossom_contraction(vector<vector<int>> &adj, int s, int u, int v){  
+  vector<bool> in_blssm(adj.size(), false);
   int lca = LCA(s, u, v);
-    for(int i=0;i<in_blossom.size();i++)in_blossom[i]=false;
-  mark_blossom(lca, u);
-  mark_blossom(lca, v);
-  if (blossom[u] != lca)
-    parent[u] = v;
-  if (blossom[v] != lca)
-    parent[v] = u;
-  for (int u = 0; u < V; u++)
-    if (in_blossom[blossom[u]])
-    {
-      blossom[u] = lca;
-      if (!BFS_Tree[u])
-      {
-          q.push(u);
-        BFS_Tree[u] = true;
+  mark_blossom(in_blssm, lca, u);
+  mark_blossom(in_blssm, lca, v);
+  if (blossom[u] != lca)  parent[u] = v;
+  if (blossom[v] != lca)  parent[v] = u;
+  
+  for (int i = 0; i < adj.size(); i++)
+    if (in_blssm[blossom[i]]) {
+      blossom[i] = lca;
+      if (!BFS_Tree[u]) {
+        q.push(i);
+        BFS_Tree[i] = true;
       }
     }
 }
 
-int find_augmenting_path(int source){
-    for (int i = 0; i < V; i++) blossom[i] = i;
+int find_augmenting_path( vector<vector<int>> &adj, int source){
+  for (int i = 0; i < adj.size(); i++) {
+    BFS_Tree[i] = false;
+    blossom[i] = i;
+    parent[i] = -1;
+  }
+  q = queue<int>(); 
+  q.push(source);
+  BFS_Tree[source] = true;
 
-    q.push(source);
-    BFS_Tree[source] = true;
+  while ( !q.empty() ) {
+    int u = q.front();
+    q.pop();
 
-    while ( !q.empty() ) {
-      int u = q.front();
-      q.pop();
-
-      for (struct_edge * e = adj[u]; e; e = e->n) {
-        int v = e->v;
-        if (blossom[u] != blossom[v] && match[u] != v)
-        {
-          if ((v == source) || (match[v] != -1 && parent[match[v]] != -1))
-          {
-            blossom_contraction(source, u, v);
-          }
-          else if (parent[v] == -1)
-          {
-              parent[v] = u;
-              if (match[v] == -1)
-              {
-                  return v;
-              }
-              else if (!BFS_Tree[match[v]])
-              {
-                  q.push(match[v]);
-                BFS_Tree[match[v]] = true;
-              }
+    for ( int v : adj[u] ) {
+      if (blossom[u] != blossom[v] && match[u] != v){
+        if ((v == source) || (match[v] != -1 && parent[match[v]] != -1))
+          blossom_contraction(adj, source, u, v);
+        else if (parent[v] == -1) {
+          parent[v] = u;
+          if (match[v] == -1) return v;
+          else if (!BFS_Tree[match[v]]){
+            q.push(match[v]);
+            BFS_Tree[match[v]] = true;
           }
         }
       }
+    }
   }
   return -1;
 }
 
-int augment_path(int s, int t){
-  int u = t, v, w;
-  while (u != -1)
-  {
-    v = parent[u];
-    w = match[v];
-    match[v] = u;
-    match[u] = v;
+void invert_path(int s, int u){
+  while (u != -1) {
+    int w = match[parent[u]];
+    match[u] = parent[u];
+    match[parent[u]] = u;
     u = w;
   }
-  return t != -1;
 }
 
-void edmonds(){
-    int max_matching = 0;
+void edmonds(vector<vector<int>> &adj){
+  for (int r = 0; r < adj.size(); r++)
+    if (match[r] == -1) invert_path(r, find_augmenting_path(adj, r));
 
-    for (int u = 0; u < V; u++){
-        if (match[u] == -1) max_matching += augment_path(u, find_augmenting_path(u));
-    }
-    cout<<"Maximum Matching: "<<endl;
-    for (int i = 0; i < V; i++){
-        if (i < match[i]) cout << i + 1 << " " << match[i] + 1 << endl;
-    }
+  cout<<"Maximum Matching: "<<endl;
+
+  for (int i = 0; i < adj.size(); i++)
+    if (i < match[i]) cout << i + 1 << " " << match[i] + 1 << endl;
 }
 
 int main(){
-    freopen("input.txt", "r", stdin);
+  freopen("input.txt", "r", stdin);
 	// freopen("output.txt", "w", stdout);
 
-    cout<<"\nVertices are labelled from 1 to N\n"
-        <<"INPUT FORMAT for 'input.txt'\n"
-        <<"\tN M \n"
-        <<"\te_1 {u_1 v_1}\n"
-        <<"\te_2\n\t.\n\t.\n\t.\n"
-        <<"\te_M\n\n";
-
-    int N, M;
-    cin >> V;
-    cin >> M;
+  cout<<"\nVertices are labelled from 1 to N\n"
+    <<"INPUT FORMAT for 'input.txt'\n"
+    <<"\tN M \n"
+    <<"\te_1 {u_1 v_1}\n"
+    <<"\te_2\n\t.\n\t.\n\t.\n"
+    <<"\te_M\n\n";
+  
+  int X, N, M;
+  while( cin >> X >> N >> M ){
+    vector<vector<int>> adj(N);
 
     for(int i=0;i<M;i++){
-        int u, v;
-        cin >> u >> v;
-        add_edge(u - 1, v - 1);
+      int u, v;
+      cin >> u >> v;
+      adj[u - 1].push_back(v - 1);
+      adj[v - 1].push_back(u - 1);
     }
-    edmonds();
+    cout << "\t"<<X<<endl;
+    edmonds(adj);
+    for(int i=0;i<N;i++){
+      q.pop();
+      match[i]=-1, parent[i]-1, BFS_Tree[i]=false;
+    }
+  }
 }
