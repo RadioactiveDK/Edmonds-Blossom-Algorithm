15f0f90093e472b061f8efcff2be48df9bc917a8
Dhruv Kukreja <94745898+RadioactiveDK@users.noreply.github.com>
2023-11-10 14:05:46 +0530

    Update blossom.cpp

--- blossom.cpp
+++ blossom.cpp
@@ -2,47 +2,43 @@
 using namespace std;
 
 const int M = 50;
-
-queue<int> q;
-vector<int> match(M,-1), parent(M,-1), blossom(M);
-vector<bool> BFS_Tree(M,false);
+vector<int> match(M,-1), parent(M,-1), Representative(M);
 
 int LCA(int root, int u, int v){
   vector<bool> vis(M,false);
   vis[u]=true;
   while (1)  {
-    u = blossom[u];
+    u = Representative[u];
     vis[u] = true;
     if (u == root) break;
     u = parent[match[u]];
   }
   while (1){
-    v = blossom[v];
+    v = Representative[v];
     if (vis[v])  return v;
     else v = parent[match[v]];
   }
 }
 
-void mark_blossom(vector<bool> &in_blssm, int lca, int u){
-  while (blossom[u] != lca)  {
+void mark_blssm(vector<bool> &in_blssm, int lca, int u){
+  while (Representative[u] != lca)  {
     int v = match[u];
-    in_blssm[blossom[u]] = in_blssm[blossom[v]] = true;
+    in_blssm[Representative[u]] = in_blssm[Representative[v]] = true;
     u = parent[v];
-    // if (blossom[u] != lca)  parent[u] = v;
   }
 }
 
-void blossom_contraction(vector<vector<int>> &adj, int s, int u, int v){  
+void Contract_Blssm(vector<vector<int>> &adj, queue<int> &q, vector<bool> &BFS_Tree, int s, int u, int v){  
   vector<bool> in_blssm(adj.size(), false);
   int lca = LCA(s, u, v);
-  mark_blossom(in_blssm, lca, u);
-  mark_blossom(in_blssm, lca, v);
-  if (blossom[u] != lca)  parent[u] = v;
-  if (blossom[v] != lca)  parent[v] = u;
+  mark_blssm(in_blssm, lca, u);
+  mark_blssm(in_blssm, lca, v);
+  if (Representative[u] != lca)  parent[u] = v;
+  if (Representative[v] != lca)  parent[v] = u;
   
   for (int i = 0; i < adj.size(); i++)
-    if (in_blssm[blossom[i]]) {
-      blossom[i] = lca;
+    if (in_blssm[Representative[i]]) {
+      Representative[i] = lca;
       if (!BFS_Tree[u]) {
         q.push(i);
         BFS_Tree[i] = true;
@@ -52,12 +48,14 @@ void blossom_contraction(vector<vector<int>> &adj, int s, int u, int v){
 
 int find_augmenting_path( vector<vector<int>> &adj, int source){
   for (int i = 0; i < adj.size(); i++) {
-    BFS_Tree[i] = false;
-    blossom[i] = i;
+    Representative[i] = i;
     parent[i] = -1;
   }
-  q = queue<int>(); 
+
+  queue<int> q; 
   q.push(source);
+
+  vector<bool> BFS_Tree(adj.size(), false);
   BFS_Tree[source] = true;
 
   while ( !q.empty() ) {
@@ -65,9 +63,9 @@ int find_augmenting_path( vector<vector<int>> &adj, int source){
     q.pop();
 
     for ( int v : adj[u] ) {
-      if (blossom[u] != blossom[v] && match[u] != v){
+      if (Representative[u] != Representative[v] && match[u] != v){
         if ((v == source) || (match[v] != -1 && parent[match[v]] != -1))
-          blossom_contraction(adj, source, u, v);
+          Contract_Blssm(adj, q, BFS_Tree, source, u, v);
         else if (parent[v] == -1) {
           parent[v] = u;
           if (match[v] == -1) return v;
@@ -92,6 +90,12 @@ void invert_path( int u){
 }
 
 void edmonds(vector<vector<int>> &adj){
+  
+  for(int i=0;i<adj.size();i++){
+    match[i]=-1;
+    parent[i]=-1;
+  }
+
   for (int r = 0; r < adj.size(); r++)
     if (match[r] == -1) invert_path(find_augmenting_path(adj, r));
 
@@ -124,9 +128,5 @@ int main(){
     }
     cout << "\t"<<X<<endl;
     edmonds(adj);
-    for(int i=0;i<N;i++){
-      q.pop();
-      match[i]=-1, parent[i]-1, BFS_Tree[i]=false;
-    }
   }
 }
